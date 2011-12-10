d95e080fecb75fa3775536672927d8dee3194e0b
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2011-12-11 01:45:20 +0800

    Fix Facebook share

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -2,7 +2,7 @@ class InvitesController < ApplicationController
 
   def new
     @token = params[:token]
-    @goal = Goal.find_by_token(params[:token])
-	@goal = Goal.find_by_token(params[:token])
+    @goal = Goal.find_by_token(@token)
+    cookies[:token] = @token
   end
 end
