ea817c859d1136dd8847a575fd4263d7bf406eec
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2011-12-11 05:21:17 +0800

    Add city model

---
+++ spec/factories/cities.rb
@@ -0,0 +1,9 @@
+# Read about factories at http://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :city do
+    name "MyString"
+    longitude "MyString"
+    latitude "MyString"
+  end
+end
