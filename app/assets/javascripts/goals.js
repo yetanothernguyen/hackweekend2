9c848472bc57e434e5fe2129cb939e9afb4c0fb3
Gaddafi Rusli <flisterz@gaddafi.(none)>
2011-12-11 02:16:19 +0800

    anything and everything

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -1,2 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+ 	$("#landing").fadeOut(2000);
+
+ 	$("#showdropdown").click(function(e){
+ 		e.preventDefault();
+ 		$(".droplocation").removeClass("hide");
+ 		$("span.current").hide();
+ 	});
+});
