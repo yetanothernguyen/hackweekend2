680bbffa386c85af22cb95a0f16300b0703e2279
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2011-12-11 01:52:31 +0800

    Invitation show

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -5,4 +5,8 @@ class InvitesController < ApplicationController
     @goal = Goal.find_by_token(@token)
     cookies[:token] = @token
   end
+
+  def show
+    @goal = Goal.find_by_token(params[:id])
+  end
 end
