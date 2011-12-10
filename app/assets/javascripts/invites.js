1bf0ffb889e5c5f5891f1bec68455ce26aa52951
Nguyen Vu Nguyen <nvunguyen@gmail.com>
2011-12-10 23:39:01 +0800

    Merge conflict

--- app/assets/javascripts/invites.js
+++ app/assets/javascripts/invites.js
@@ -1,2 +1,31 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  $("#facebook-post").click(function() {
+    FB.ui(
+      {
+       method: 'feed',
+       message: 'Do things with people near you',
+       name: 'Grupi.co',
+       caption: 'Do things with people near you',
+       description: (
+          'Do things with people near you'
+       ),
+       link: 'http://www.fbrell.com/',
+       picture: 'http://www.fbrell.com/f8.jpg',
+       actions: [
+            { name: 'fbrell', link: 'http://www.fbrell.com/' }
+       ],
+       user_message_prompt: 'Invite your friends to do things'
+      },
+      function(response) {
+        if (response && response.post_id) {
+          alert('Post was published.');
+        } else {
+          alert('Post was not published.');
+        }
+      }
+    );
+  });
+});
